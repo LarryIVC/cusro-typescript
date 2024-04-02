e3254bf190850e100c373f096c50e7be3b1cdd31
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-01 23:45:37 -0400

    Add data types and function with annotations

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log('my name is Jhon')
+// let num:number = 20
+// // num= 'Jhon'
+// num=55
+// let age:number = 200
+// const age:number = 200
+// // age = 400
+// const value:string = 'Jhon'
+// console.log(age, value + 100)
+//datatypes
+// String
+// Number
+// Boolean
+// Array
+// null
+// undefined
+// Object
+// let statement:boolean = true
+// console.log(statement)
+// const names:string[] = ['Jhon', 'Doe', 'Smith']
+// console.log(names)
+// const user: {name:string, age:number} = {
+//   name: 'Jhon',
+//   age: 20
+// }
+// console.log(user)
+//Functions with anotations
+// let a:number | string = "20"
+var a = true;
+console.log(a);
+// function userId(x:number):number {
+var userId = function (x) {
+    return 20 + 30 + x;
+};
+console.log(userId(10));
