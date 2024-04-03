cb6c7a17b8df7a98d90ff352250ba2c22cb62877
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-02 23:19:45 -0400

    Refactor code and add array operations

--- index.ts
+++ index.ts
@@ -37,12 +37,66 @@ export {}
 //Functions with anotations
 
 // let a:number | string = "20"
-let a:any = true
-console.log(a)
+// let a:any = true
+// console.log(a)
 
 // function userId(x:number):number {
-const userId = (x:number):number => {
-  return 20 +30 +x
-}
+// const userId = (x:number):number => {
+//   return 20 +30 +x
+// }
 
-console.log(userId(10))
+// console.log(userId(10))
+
+// Interfaces
+
+// interface User { Username: string; Age: number; email: string, UserId: () => string}
+
+// const person: User = {
+//   Username: 'Jhon',
+//   Age: 20,
+//   email: 'jhon@gmail.com',
+//   UserId: function() {
+//     return "Jhon"
+//   }
+// }
+
+
+// console.log(person)
+
+//Classes
+
+// class user {
+  
+//   name : string
+//   constructor(name) {
+//     this.name = name
+//   }
+//   getName() {
+//     console.log(this.name)
+//   }
+// }
+
+// const person = new user('Larry')
+// person.getName()
+
+// Arrays
+
+let colors: string[] = ['red', 'green', 'blue']
+colors.push('yellow')
+console.log(colors)
+
+let shifted = colors.shift()
+console.log(shifted)
+
+colors.unshift('black')
+console.log(colors)
+
+let array1: number[] = [1,2,3,4,5]
+let array2: number[] = [6,7,8,9,10]
+let array3: number[] = array1.concat(array2)
+
+console.log(array3)
+
+let sliced: string[] = colors.slice(1,3)
+
+console.log(sliced)
