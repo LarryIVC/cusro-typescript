8c5e2d4f0cc427015ddd019d07716b433299e4cc
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-02 23:19:41 -0400

    Refactor code and add examples for arrays

--- index.js
+++ index.js
@@ -28,10 +28,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // console.log(user)
 //Functions with anotations
 // let a:number | string = "20"
-var a = true;
-console.log(a);
+// let a:any = true
+// console.log(a)
 // function userId(x:number):number {
-var userId = function (x) {
-    return 20 + 30 + x;
-};
-console.log(userId(10));
+// const userId = (x:number):number => {
+//   return 20 +30 +x
+// }
+// console.log(userId(10))
+// Interfaces
+// interface User { Username: string; Age: number; email: string, UserId: () => string}
+// const person: User = {
+//   Username: 'Jhon',
+//   Age: 20,
+//   email: 'jhon@gmail.com',
+//   UserId: function() {
+//     return "Jhon"
+//   }
+// }
+// console.log(person)
+//Classes
+// class user {
+//   name : string
+//   constructor(name) {
+//     this.name = name
+//   }
+//   getName() {
+//     console.log(this.name)
+//   }
+// }
+// const person = new user('Larry')
+// person.getName()
+// Arrays
+var colors = ['red', 'green', 'blue'];
+colors.push('yellow');
+console.log(colors);
+var shifted = colors.shift();
+console.log(shifted);
+colors.unshift('black');
+console.log(colors);
+var array1 = [1, 2, 3, 4, 5];
+var array2 = [6, 7, 8, 9, 10];
+var array3 = array1.concat(array2);
+console.log(array3);
+var sliced = colors.slice(1, 3);
+console.log(sliced);
