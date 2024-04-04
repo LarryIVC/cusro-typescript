2c63555ade9c5ae77a33d181c6cd1d7a533caec9
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-04 00:56:44 -0400

    Add Signup class to build folder

---
+++ build/signup.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Signup {
+    constructor() {
+        this.data = "Signup successfull";
+        this.name = 'Carlos';
+    }
+}
+exports.default = Signup;
