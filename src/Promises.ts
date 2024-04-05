b8719a49c58f050a5424ffe9047ea90f0996a359
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-05 00:40:13 -0400

    Add Promises and fetchData functions

--- src/Promises.ts
+++ src/Promises.ts
@@ -26,3 +26,49 @@ user1.setMyName('Doe')
 user1.displayName()
 // user1.getLength() -> no se puede por private
 
+class Person {
+  name: string
+  age: number
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  greeting() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+  }
+}
+
+let person1 = new Person('Jhon', 30)
+person1.greeting()
+
+function fetchData(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = 'This is the fetched data'
+      resolve(data)
+    }, 2000)
+  }
+  )
+}
+
+fetchData()
+  .then(data => {
+    console.log("Data received: ", data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  // Promises whit async/await
+
+  async function fetchDataAsync() {
+    try {
+      let data = await fetchData()
+      console.log("Data received with async await: ", data)      
+    } catch (error) {
+      console.log(error) 
+    }
+  }
+
+  fetchDataAsync()
